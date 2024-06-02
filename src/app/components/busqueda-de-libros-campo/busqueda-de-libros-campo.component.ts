48c8f6b65aaa6a01c7d92f8b39fbac2a7a40ae1e
ialmanza <isra.almanzagonzalez@gmail.com>
2024-06-02 03:11:02 -0300

    Se agrega paginación en el listar

--- src/app/components/busqueda-de-libros-campo/busqueda-de-libros-campo.component.ts
+++ src/app/components/busqueda-de-libros-campo/busqueda-de-libros-campo.component.ts
@@ -21,13 +21,21 @@ export class BusquedaDeLibrosCampoComponent {
   libros: Libro[] = [];
   filteredLibros: Libro[] = [];
   searchTerm: string = '';
+  displayedLibros: Libro[] = [];
+
+  pageSizeOptions = [5, 10, 20];
+  pageSize = this.pageSizeOptions[0];
+  currentPage = 0;
+  totalItems = 0;
 
   constructor(private librosServicio: LibrosServicioService, private dialog: MatDialog) {}
 
   ngOnInit() {
     this.librosServicio.getLibros().subscribe((libros: Libro[]) => {
       this.libros = libros;
-      this.filteredLibros = libros
+      this.filteredLibros = libros;
+      this.totalItems = libros.length;
+      this.updateDisplayedLibros();
     });
   }
 
@@ -38,8 +46,45 @@ export class BusquedaDeLibrosCampoComponent {
       libro.genero.toLowerCase().includes(query.toLowerCase()) ||
       libro.editorial.toLowerCase().includes(query.toLowerCase()) ||
       libro.fechapublicacion.toLowerCase().includes(query.toLowerCase())
-
     );
+    this.totalItems = this.filteredLibros.length;
+    this.currentPage = 0;
+    this.updateDisplayedLibros();
+  }
+
+  updateDisplayedLibros() {
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.displayedLibros = this.filteredLibros.slice(start, end);
+  }
+
+  changePageSize(size: number) {
+    this.pageSize = size;
+    this.currentPage = 0;
+    this.updateDisplayedLibros();
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.updateDisplayedLibros();
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages() - 1) {
+      this.currentPage++;
+      this.updateDisplayedLibros();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.updateDisplayedLibros();
+    }
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
   }
 
 
--- src/app/components/listar-con-tabla/listar-con-tabla.component.ts
+++ src/app/components/listar-con-tabla/listar-con-tabla.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { LibroComponent } from "../libro/libro.component";
 import { LibrosServicioService } from '../../services/libros-servicio.service';
 import { Libro } from '../../models/Libro';
@@ -9,6 +9,7 @@ import { DialogContentExampleDialog } from "../ventana-modal-mostrar-libro/venta
 import { DialogContentEditExampleDialog } from '../ventana-modal-editar-libro/ventana-modal-editar-libro.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAnimationsExampleDialog } from '../ventana-modal/ventana-modal.component';
+// import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-listar-con-tabla',
@@ -22,13 +23,21 @@ export class ListarConTablaComponent {
   libros: Libro[] = [];
   filteredLibros: Libro[] = [];
   searchTerm: string = '';
+  displayedLibros: Libro[] = [];
+
+  pageSizeOptions = [5, 10, 20];
+  pageSize = this.pageSizeOptions[0];
+  currentPage = 0;
+  totalItems = 0;
 
   constructor(private librosServicio: LibrosServicioService, private dialog: MatDialog) {}
 
   ngOnInit() {
     this.librosServicio.getLibros().subscribe((libros: Libro[]) => {
       this.libros = libros;
-      this.filteredLibros = libros
+      this.filteredLibros = libros;
+      this.totalItems = libros.length;
+      this.updateDisplayedLibros();
     });
   }
 
@@ -38,7 +47,45 @@ export class ListarConTablaComponent {
       libro.primerautor.toLowerCase().includes(query.toLowerCase()) ||
       libro.genero.toLowerCase().includes(query.toLowerCase())
     );
+    this.totalItems = this.filteredLibros.length;
+    this.currentPage = 0;
+    this.updateDisplayedLibros();
   }
+  updateDisplayedLibros() {
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.displayedLibros = this.filteredLibros.slice(start, end);
+  }
+
+  changePageSize(size: number) {
+    this.pageSize = size;
+    this.currentPage = 0;
+    this.updateDisplayedLibros();
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.updateDisplayedLibros();
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages() - 1) {
+      this.currentPage++;
+      this.updateDisplayedLibros();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.updateDisplayedLibros();
+    }
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
+  }
+
 
   openEditDialog(libro: Libro) {
     const dialogRef = this.dialog.open(DialogContentEditExampleDialog, {
@@ -64,4 +111,27 @@ export class ListarConTablaComponent {
       }
     });
   }
+
+
+  // exportToExcel(): void {
+  //   const filteredData = this.libros.map(libro => {
+  //     const { titulo, isbn, primerautor, segundoautor, tercerautor, fechapublicacion, editorial, genero, paginas, descripcion, ...rest } = libro;
+  //     return {
+  //       Título: titulo,
+  //       ISBN: isbn,
+  //       'Primer autor': primerautor,
+  //       'Segundo autor': segundoautor,
+  //       'Tercer autor': tercerautor,
+  //       'Fecha de publicación': fechapublicacion,
+  //       Editorial: editorial,
+  //       Género: genero,
+  //       Páginas: paginas,
+  //       'Campo adicional': descripcion
+  //     };
+  //   });
+  //   const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(filteredData);
+  //   const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, 'Libros');
+  //   XLSX.writeFile(wb, 'Libros.xlsx');
+  // }
 }
