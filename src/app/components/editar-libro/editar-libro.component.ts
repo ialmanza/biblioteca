a9c15172204d73cfe2a80878d0cf2c2934f25e7b
ialmanza <isra.almanzagonzalez@gmail.com>
2024-06-06 10:59:14 -0300

    editar libro creado

---
+++ src/app/components/editar-libro/editar-libro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-libro',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-libro.component.html',
+  styleUrl: './editar-libro.component.css'
+})
+export class EditarLibroComponent {
+
+}
